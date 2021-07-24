f39c33ab85866537095ac6bfb31b69972c335d58
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-07-24 18:07:59 +0900

    0724 설정중
    스프링 설정

---
+++ CatchMind/src/main/java/com/spring/catchmind/member/MemberDto.java
@@ -0,0 +1,17 @@
+package com.spring.catchmind.member;
+
+public class MemberDto {
+	
+	private int member_no;
+	private String member_id;
+	private String member_pw;
+	private int member_age;
+	private String member_gender;
+	private String member_email;
+	private String member_enabled;
+	private String member_payrole;
+	private String member_role;
+	private String member_type;
+	private String member_blist;
+	
+}

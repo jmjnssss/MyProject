bbf0c43ae9740c3f1662ec40ded63ae64d5f29d7
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-07-26 19:59:07 +0900

    0726
    로그인 컨트롤러

---
+++ CatchMind/src/main/java/com/spring/catchmind/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.spring.catchmind.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+	public String loginform() {
+		return null;
+	}
+}

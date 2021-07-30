7a4ad1099c93782ef8149177d3f1b82733656c2a
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-07-30 19:42:49 +0900

    0730 로그인
    로그인

--- CatchMind/src/main/java/com/spring/catchmind/member/LoginDao.java
+++ CatchMind/src/main/java/com/spring/catchmind/member/LoginDao.java
@@ -2,4 +2,11 @@ package com.spring.catchmind.member;
 
 public interface LoginDao {
 
+	String NAMESPACE = "memberMapper.";
+	
+	// 로그인
+	public MemberDto login(MemberDto dto);
+	
+	// 회원가입
+	public int join(MemberDto dto);
 }
--- CatchMind/src/main/java/com/spring/catchmind/member/LoginDaoImpl.java
+++ CatchMind/src/main/java/com/spring/catchmind/member/LoginDaoImpl.java
@@ -1,5 +1,29 @@
 package com.spring.catchmind.member;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LoginDaoImpl implements LoginDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	@Override
+	public MemberDto login(MemberDto dto) {
+		
+		MemberDto res = null;
+		
+		
+		return res;
+	}
+
+	@Override
+	public int join(MemberDto dto) {
+		
+		int res = 0;
+		
+		return res;
+	}
 
 }

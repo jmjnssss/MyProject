0f1c28d8e260fd08f16d97b1fdb5486fbf643fac
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-08-02 23:02:14 +0900

    0802 게시판
    게시판 dto

--- CatchMind/src/main/java/com/spring/catchmind/Board/BoardDto.java
+++ CatchMind/src/main/java/com/spring/catchmind/Board/BoardDto.java
@@ -1,5 +1,24 @@
 package com.spring.catchmind.Board;
 
+import java.sql.Date;
+
 public class BoardDto {
 
+	private int post_id;
+	private int board_id;
+	private int post_no;
+	private String post_category;
+	private String post_writer;
+	private String post_title;
+	private String post_content;
+	private Date post_regdate;
+	private int post_hit;
+	private int post_comment_count;
+	private String post_delflag;
+	private String originName;
+	private String changeName;
+	
+	public BoardDto() {
+	}
+	
 }

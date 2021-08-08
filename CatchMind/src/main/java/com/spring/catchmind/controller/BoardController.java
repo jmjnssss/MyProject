459f63f71105f315255ddd2f7aaf6e1fac463d16
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-08-08 17:34:23 +0900

    0808 컨트롤러
    컨트롤러 접근

--- CatchMind/src/main/java/com/spring/catchmind/controller/BoardController.java
+++ CatchMind/src/main/java/com/spring/catchmind/controller/BoardController.java
@@ -6,10 +6,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardController {
 
+	// 게시판
 	@RequestMapping("/boardlist.do")
 	public String board() {
 		return "board/boardlist";
 	}
+	
+	// 공지사항
+	@RequestMapping("/noticeboardDetail.do")
+	public String noticeboardDetail() {
+		return "board/noticeboardDetail";
+	}
+	
+	// 공지 작성
+	@RequestMapping("/noticeboardInsert.do")
+	public String noticeboardInsert() {
+		return "board/noticeboardInsert";
+	}
+	
+	// 공지 수정
+	@RequestMapping("/noticeboardUpdate.do")
+	public String noticeboardUpdate() {
+		return "board/noticeboardUpdate";
+	}
+	
+	// 문의게시판 글보기
+	@RequestMapping("/qnaboardDetail.do")
+	public String qnaboardDetail() {
+		return "board/qnaboardDetail";
+	}
+	
+	// 문의 작성
+	@RequestMapping("/qnaboardInsert.do")
+	public String qnaboardInsert() {
+		return "board/qnaboardInsert";
+	}
+	
+	// 문의게시판
+	@RequestMapping("/qnaboardList.do")
+	public String qnaboardList() {
+		return "board/qnaboardList";
+	}
+	
+	// 문의 수정
+	@RequestMapping("/qnaboardUpdate.do")
+	public String qnaboardUpdate() {
+		return "board/qnaboardUpdate";
+	}
+	
+	// 에러페이지
+	@RequestMapping("/errorPage.do")
+	public String errorPage() {
+		return "board/errorPage";
+	}
+	
 	
 	
 }
--- CatchMind/src/main/java/com/spring/catchmind/controller/LoginController.java
+++ CatchMind/src/main/java/com/spring/catchmind/controller/LoginController.java
@@ -17,4 +17,22 @@ public class LoginController {
 	public String joinForm() {
 		return "member/joinform";
 	}
+	
+	// 아이디 찾기
+	@RequestMapping("/findIdForm.do")
+	public String findIdForm() {
+		return "member/findIdForm";
+	}
+	
+	// 비밀번호 찾기
+	@RequestMapping("/findPwForm.do")
+	public String findPwForm() {
+		return "member/findPwForm";
+	}
+	
+	// 비밀번호 변경
+	@RequestMapping("/updatePwForm.do")
+	public String updatePwForm() {
+		return "member/updatePwForm";
+	}
 }

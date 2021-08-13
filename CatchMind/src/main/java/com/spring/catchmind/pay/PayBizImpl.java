c3713526c3835607201d3a329fb4abccfa98fbbb
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-08-13 20:19:25 +0900

    0813 pay dto
    pay dto

--- CatchMind/src/main/java/com/spring/catchmind/pay/PayBizImpl.java
+++ CatchMind/src/main/java/com/spring/catchmind/pay/PayBizImpl.java
@@ -1,5 +1,8 @@
 package com.spring.catchmind.pay;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PayBizImpl implements PayBiz {
 
 }
--- CatchMind/src/main/java/com/spring/catchmind/pay/PayDaoImpl.java
+++ CatchMind/src/main/java/com/spring/catchmind/pay/PayDaoImpl.java
@@ -1,5 +1,8 @@
 package com.spring.catchmind.pay;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PayDaoImpl implements PayDao {
 
 }

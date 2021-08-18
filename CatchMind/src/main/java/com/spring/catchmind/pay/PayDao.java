39e5e84ae0c5dd903a68a400f5c73e6585ff5b8b
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-08-18 16:27:38 +0900

    0818 pay
    pay

--- CatchMind/src/main/java/com/spring/catchmind/pay/PayDao.java
+++ CatchMind/src/main/java/com/spring/catchmind/pay/PayDao.java
@@ -3,4 +3,6 @@ package com.spring.catchmind.pay;
 public interface PayDao {
 
 	static String NAMESPACE = "payMapper.";
+
+	public int insertPay(PayDto dto);
 }
--- CatchMind/src/main/java/com/spring/catchmind/pay/PayDaoImpl.java
+++ CatchMind/src/main/java/com/spring/catchmind/pay/PayDaoImpl.java
@@ -1,8 +1,25 @@
 package com.spring.catchmind.pay;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PayDaoImpl implements PayDao {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public int insertPay(PayDto dto) {
+		int res = 0;
+		
+		try {
+			res= sqlSession.insert(NAMESPACE+"insertpay",dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	return res;
+	}
 }

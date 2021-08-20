9594c02f83ad72483c1f14324ef6eeb2a2e4fa02
jmjnssss <79584942+jmjnssss@users.noreply.github.com>
2021-08-20 22:03:05 +0900

    0820 loginbiz
    loginbiz,bizimpl

--- CatchMind/src/main/java/com/spring/catchmind/member/LoginBiz.java
+++ CatchMind/src/main/java/com/spring/catchmind/member/LoginBiz.java
@@ -1,5 +1,41 @@
 package com.spring.catchmind.member;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 public interface LoginBiz {
+
+	// 로그인
+	public MemberDto login(MemberDto dto);
+	
+	// sns로그인
+	public MemberDto snslogin(String member_email);
+	
+	// 회원가입
+	public int join(MemberDto dto);
+	
+	// 비밀번호 변경
+	public int updatePw(MemberDto dto);
+	
+	// 회원 탈퇴
+	public int leaveAccount(MemberDto dto);
+	
+	// 아이디 중복 검사
+	public int idCheck(MemberDto dto);
+	
+	// 이메일 중복 검사
+	public int emailCheck(MemberDto dto);
+	
+	// 아이디 찾기
+	public MemberDto findId(MemberDto dto);
+	
+	// sns token chk
+	public int tokenchk(String member_email);
+	
+	//비밀번호 찾기
+	public void findPw(HttpServletResponse response, MemberDto dto);
+
+	public String getPw(Map<String, Object> paramMap);
 
 }
--- CatchMind/src/main/java/com/spring/catchmind/member/LoginBizImpl.java
+++ CatchMind/src/main/java/com/spring/catchmind/member/LoginBizImpl.java
@@ -1,5 +1,64 @@
 package com.spring.catchmind.member;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 public class LoginBizImpl implements LoginBiz {
+
+	@Override
+	public MemberDto login(MemberDto dto) {
+		return null;
+	}
+
+	@Override
+	public MemberDto snslogin(String member_email) {
+		return null;
+	}
+
+	@Override
+	public int join(MemberDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int updatePw(MemberDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int leaveAccount(MemberDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int idCheck(MemberDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int emailCheck(MemberDto dto) {
+		return 0;
+	}
+
+	@Override
+	public MemberDto findId(MemberDto dto) {
+		return null;
+	}
+
+	@Override
+	public int tokenchk(String member_email) {
+		return 0;
+	}
+
+	@Override
+	public void findPw(HttpServletResponse response, MemberDto dto) {
+		
+	}
+
+	@Override
+	public String getPw(Map<String, Object> paramMap) {
+		return null;
+	}
 
 }
